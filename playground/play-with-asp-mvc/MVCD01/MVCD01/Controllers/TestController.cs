0cb4fe2583e3011377fe0d39a987ee9cfda32d73
Mahmoud Gadallah <gadallah@std.mans.edu.eg>
2025-03-06 19:07:26 +0200

    feat: add mvc d01 task

---
+++ playground/play-with-asp-mvc/MVCD01/MVCD01/Controllers/TestController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCD01.Models;
+
+namespace MVCD01.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Display()
+        {
+            return "Welcom MVC!";
+        }
+
+        public IActionResult Show()
+        {
+            return View();
+        }
+
+        public IActionResult ShowAdd()
+        {
+            return View();
+        }
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public IActionResult CreateStd()
+        {
+            return View();
+        }
+
+        public string AddStd(Student std)
+        {
+            return new Student() { Id = new Random().Next(1, 10), Name = std.Name, Age = std.Age }.ToString();
+        }
+    }
+}
---
+++ playground/play-with-asp-mvc/MVCD01/MVCD01/Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace MVCD01.Models;
+
+public record Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
