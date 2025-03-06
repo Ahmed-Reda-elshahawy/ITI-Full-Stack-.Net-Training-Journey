75353ed9c75f941c78abdeaf2d66f37a8ff9c8d8
Mahmoud Gadallah <gadallah@std.mans.edu.eg>
2025-03-06 19:09:01 +0200

    feat: add mvc d03 task

---
+++ playground/play-with-asp-mvc/MVCD03/Solution1/WebApplication1/Controllers/DepartmentController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Database;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private MVCD03DbContext _db = new MVCD03DbContext();
+        public IActionResult Index()
+        {
+            return View(_db.Departments.ToList());
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Department dept)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(dept);
+            }
+            _db.Departments.Add(dept);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var dept = _db.Departments.FirstOrDefault(d => d.Id == id);
+
+            if (dept == null)
+            {
+                return NotFound();
+            }
+
+            return View(dept);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Department dept)
+        {
+            if (!ModelState.IsValid)
+                return View(dept);
+            _db.Departments.Update(dept);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var dept = _db.Departments.Include(d => d.Students).FirstOrDefault(d => d.Id == id);
+
+            if (dept == null)
+            {
+                return NotFound();
+            }
+
+            if (dept.Students.Count() <= 0)
+            {
+                _db.Departments.Remove(dept);
+                _db.SaveChanges();
+                TempData["SuccessMessage"] = "Department deleted successfully.";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Can't Delete Department, because there's students enrolled!";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult CheckNameExistance(string name, int? id)
+        {
+            bool nameExists = id == null
+                ? _db.Departments.Any(d => d.Name == name)
+                : _db.Departments.Any(d => d.Name == name && d.Id != id);
+
+            if (nameExists)
+            {
+                return Json($"The name '{name}' is already exist.");
+            }
+
+            return Json(true);
+        }
+    }
+}
---
+++ playground/play-with-asp-mvc/MVCD03/Solution1/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using WebApplication1.Database;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        private MVCD03DbContext _db = new MVCD03DbContext();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var stds = _db.Students.Include(s => s.Department).ToList();
+
+            return View(stds);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Departments = _db.Departments.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student std)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(std);
+            }
+            _db.Students.Add(std);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var std = _db.Students.FirstOrDefault(s => s.Id == id);
+
+            if (std == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.Departments = _db.Departments.ToList();
+
+            return View(std);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Student std)
+        {
+            if (!ModelState.IsValid)
+                return View(std);
+            _db.Students.Update(std);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var std = _db.Students.FirstOrDefault(s => s.Id == id);
+
+            if (std == null)
+            {
+                return NotFound();
+            }
+
+            _db.Students.Remove(std);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult CheckEmailExistence(string email, int? id)
+        {
+            bool emailExists = id == null
+                ? _db.Students.Any(s => s.Email == email)
+                : _db.Students.Any(s => s.Email == email && s.Id != id);
+
+            if (emailExists)
+            {
+                return Json($"The email '{email}' is already registered.");
+            }
+
+            return Json(true);
+        }
+
+    }
+}
